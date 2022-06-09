353a3df9ab31ff1eb3d3974c496c19bc415b403e
devydk <100238297+projIARafael@users.noreply.github.com>
2022-06-08 22:40:09 -0300

    commit

---
+++ APPApiProject/src/App.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+export default () =>{
+  return (
+    <SafeAreaView>
+      <Text>Oi</Text>
+    </SafeAreaView>
+  )
+}
